618e41918b285ed27e786a705b89292b35fb9459
Свирид Володимир <38647042+svyrydvladymyr@users.noreply.github.com>
2019-01-13 14:28:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" type="text/css" href="css/main_content_footer.css">
-    
-	<title>Art-of-medicine</title>
+   <link rel="stylesheet" type="text/css" href="css/main_content_footer.css">    
+   <title>Art-of-medicine</title>
 </head>
 
 <body>
